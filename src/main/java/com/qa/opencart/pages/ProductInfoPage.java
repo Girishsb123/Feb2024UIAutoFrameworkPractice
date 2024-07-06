bb34c6af342cab20267f1ee4e64f026457c0dab8
Girish <girishvjc1993@gmail.com>
2024-07-06 11:37:32 +0530

    updated product info page

--- src/main/java/com/qa/opencart/pages/ProductInfoPage.java
+++ src/main/java/com/qa/opencart/pages/ProductInfoPage.java
@@ -81,7 +81,7 @@ public class ProductInfoPage {
 		getProductMetaData();
 		getProductPriceData();
 		
-		System.out.println(productMap);
+		System.out.println("product info :" +productMap);
 		return productMap;
 	}
 	
